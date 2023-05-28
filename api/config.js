ddae94febc31660e873866062b24102733390195
FullyAngozi <ochuks33@yahoo.com>
2023-05-28 17:00:14 -0400

    nothig more

--- api/config.js
+++ api/config.js
@@ -5,6 +5,6 @@ module.exports = {
   MONGODB_URI:
     "mongodb+srv://ochukoangozi:96q6W4S3hoXZTMXh@cluster0.ybvmlox.mongodb.net/?retryWrites=true&w=majority",
   SecretKey: "njknc1o22339kfem2m332cfvn",
-  CORS_ORIGIN: "http://localhost:5173",
+  CORS_ORIGIN: "https://angoblog.netlify.app",
   // other environment variables
 };
